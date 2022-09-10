e328e68e9e868725ea0803528c79559f3c7cdd4b
QuarkPixel <m2462864403@gmail.com>
2022-09-10 18:29:02 +0800

    -

--- main.js
+++ main.js
@@ -6,70 +6,105 @@ var titleFocus = true,
     maxLine,
     halfHeight = window.innerHeight / 2,
     isFullScreen = false,
-    playMusic
+    playMusic,
+    parameters = {
+        colors: [
+            "29, 29, 31",
+            "22, 33, 62",
+            "250, 47, 181",
+            "83, 52, 131",
+            "233, 69, 96",
+            "15, 52, 96",
+        ],
+        info: {
+            title: "向&ensp;北&ensp;方",
+            maker: "舒&ensp;婷",
+        },
+        lines: [
+            "一朵初夏的蔷薇",
+            "划过波浪的琴弦",
+            "向不可及的水平远航",
+            "乌云像癣一样",
+            "布满天空的颜面",
+            "鸥群",
+            "却为她铺开洁白的翅膀",
+            "",
+            "去吧",
+            "我愿望的小太阳",
+            "如果你沉没了",
+            "就睡在大海的胸膛",
+            "在水母银色的帐顶",
+            "永远有绿色的波涛喧响",
+            "",
+            "让我也漂去吧",
+            "让阳光熨贴的风",
+            "把我轻轻吹送",
+            "顺着温暖的海流",
+            "漂向北方",
+            "",
+            "小组成员<br>孟轩聪&emsp;裴梓辰<br>卜梓航&emsp;葛婉怡",
+        ],
+        style: {
+            l21: "font-size: 27pt;line-height: 1.5em",
+        },
+        blur: "0px",
+        music: {
+            enable: true,
+            stop: 21,
+        },
+    }
 
 const title = document.querySelector("#title"),
     lines = document.querySelector("#lines"),
     bgm = document.querySelector("#bgm"),
     root = document.documentElement
 
-window.onload = function () {
-    let url = "Configure.json"
-    let request = new XMLHttpRequest()
-    request.open("get", url)
-    request.send(null)
-    request.onload = function () {
-        if (request.status == 200)
-            var parameters = JSON.parse(request.responseText)
+//! color
+root.style.setProperty("--colorBG", parameters.colors[0])
+root.style.setProperty("--colorI", parameters.colors[1])
+root.style.setProperty("--colorII", parameters.colors[2])
+root.style.setProperty("--colorIII", parameters.colors[3])
+root.style.setProperty("--colorVI", parameters.colors[4])
+root.style.setProperty("--colorV", parameters.colors[5])
 
-        //! color
-        root.style.setProperty("--colorBG", parameters.colors[0])
-        root.style.setProperty("--colorI", parameters.colors[1])
-        root.style.setProperty("--colorII", parameters.colors[2])
-        root.style.setProperty("--colorIII", parameters.colors[3])
-        root.style.setProperty("--colorVI", parameters.colors[4])
-        root.style.setProperty("--colorV", parameters.colors[5])
+//! blur
+root.style.setProperty("--blurValue", parameters.blur)
 
-        //! blur
-        root.style.setProperty("--blurValue", parameters.blur)
+//! title&maker
+if (parameters.info.maker) {
+    title.innerHTML =
+        "<div>" +
+        parameters.info.title +
+        "</div><p>" +
+        parameters.info.maker +
+        "</p>"
+} else {
+    title.innerHTML = "<div>" + parameters.info.title + "</div>"
+}
 
-        //! title&maker
-        if (parameters.info.maker) {
-            title.innerHTML =
-                "<div>" +
-                parameters.info.title +
-                "</div><p>" +
-                parameters.info.maker +
-                "</p>"
-        } else {
-            title.innerHTML = "<div>" + parameters.info.title + "</div>"
-        }
-
-        //! lines
-        let linesHTML = ""
-        maxLine = parameters.lines.length - 1
-        for (var i = 0; i < parameters.lines.length; i++) {
-            linesHTML += "<p>" + parameters.lines[i] + "</p>"
-            if (!parameters.lines[i]) {
-                emptyLines.push(i)
-            }
-        }
-        lines.innerHTML = linesHTML
-        line_p = lines.getElementsByTagName("p")
-        Object.keys(parameters.style).forEach(function (key) {
-            line_p[Number(key.slice(1, key.length))].setAttribute(
-                "style",
-                parameters.style[key]
-            )
-        })
-
-        //! music
-
-        if (parameters.music.enable) {
-            playMusic = true
-            stopMusic = parameters.music.stop
-        }
+//! lines
+let linesHTML = ""
+maxLine = parameters.lines.length - 1
+for (var i = 0; i < parameters.lines.length; i++) {
+    linesHTML += "<p>" + parameters.lines[i] + "</p>"
+    if (!parameters.lines[i]) {
+        emptyLines.push(i)
     }
+}
+lines.innerHTML = linesHTML
+line_p = lines.getElementsByTagName("p")
+Object.keys(parameters.style).forEach(function (key) {
+    line_p[Number(key.slice(1, key.length))].setAttribute(
+        "style",
+        parameters.style[key]
+    )
+})
+
+//! music
+
+if (parameters.music.enable) {
+    playMusic = true
+    stopMusic = parameters.music.stop
 }
 
 function nextLine() {
